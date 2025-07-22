f11427f586263bc642a907e280312645ac3a688c
mhollins <mhollins@wbgames.com>
2025-07-22 08:54:24 -0400

    Get random wolf

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+const wolfImages = [
+    "https://media.istockphoto.com/id/91895082/photo/young-male-wolf.jpg?s=612x612&w=0&k=20&c=pbl9NoJoMnW1d8hTLYSYo84uibFQbtXd48h1xhxwuiQ=",
+    "https://www.reddit.com/media?url=https%3A%2F%2Fexternal-preview.redd.it%2FFCv9LSW37bTUDDM-WykVNReJNax_JT43sWgpGC58GL4.png%3Fformat%3Dpjpg%26auto%3Dwebp%26s%3Dc7fa81004e8b3e2751e42a375074aac7c6aab42d",
+    "https://i.ytimg.com/vi/knSOtiotbB0/maxresdefault.jpg"
+]
+
+function getRandomWolf()
+{
+    return wolfImages.at( Math.random() * wolfImages.length )
+}
